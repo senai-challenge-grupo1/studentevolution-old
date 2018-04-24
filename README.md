89d2f09fb93d55d3073d058e8cc7158d5f860a5e
AlexRMS <ricdoa2@gmail.com>
2018-04-23 21:59:08 -0300

    Clear Project

--- README.md
+++
@@ -1,3 +0,0 @@
-# (Projeto Base)
-
-Criando um projeto base para outros projetos usando FrameWork Ruby on Rails.
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :cursos
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'pages#home'  
-end
