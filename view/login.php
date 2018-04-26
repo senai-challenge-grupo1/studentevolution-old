f54b2cd81cd5f61c007859f6f857d43b6e672fc7
AlexRMS <ricdoa2@gmail.com>
2018-04-25 22:27:31 -0300

    25-04

--- view/login.php
+++ view/login.php
@@ -1,6 +1,9 @@
 <div class="form">
-	<p>Login</p>
+	<p>Registrar</p>
+	<input class="form-control" type="text" ng-model="nome" placeholder="Nome">
 	<input class="form-control" type="text" ng-model="login" placeholder="Login">
+	<input class="form-control" type="text" ng-model="data_nascimento" placeholder="Data de Nascimento">
+	<input class="form-control" type="email" ng-model="email" placeholder="Email">
 	<input class="form-control" type="password" ng-model="senha" placeholder="Senha">
 	<button ng-click="logar()">Entrar</button>
 </div>
