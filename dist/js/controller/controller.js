14b11ceca3b1b3eeae2994135f8d46afe086fed2
alexrms <ricdoa2@gmail.com>
2018-04-26 08:16:59 -0300

    Removing php of the project

--- dist/js/controller/controller.js
+++ dist/js/controller/controller.js
@@ -1,19 +1,6 @@
 angular.module("StudentEvolution")
 .controller("loginCtrl", function($scope) {
-	$scope.logar = function() {
-		var resultado;
-		$.ajax({
-			method: "POST",
-			url: "http://localhost:8000/api/usuarios",
-			cache: false,
-			data: {nome: $scope.nome, login: $scope.login, data_nascimento: $scope.data_nascimento, email: $scope.email, senha: md5($scope.senha)},
-			success: function(result){
-				this.resultado = result;
-			}
-		});
-		alert(resultado);
-		window.location.reload();
-	};
+	$scope.logar = "";
 })
 .controller("homeCtrl", function($scope) {
 	$scope.msg = "Home";
